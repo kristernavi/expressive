d5bce4c0616d63acfc6e511606cf65c867fe78d8
Ivan Krister Garcia <kristernavi@gmail.com>
2017-09-20 13:25:26 +0800

    initial commit with eloquent

---
+++ config/autoload/eloquent.global.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'eloquent' => [
+        'driver'    => 'mysql',
+        'host'      => '127.0.0.1',
+        'database'  => 'nycbites',
+        'username'  => 'root',
+        'password'  => '',
+        'charset'   => 'utf8',
+        'collation' => 'utf8_general_ci',
+    ]
+];
---
+++ src/App/src/Model/User.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model as EloquentModel;
+
+class User extends EloquentModel
+{
+    protected $table = 'users';
+}
